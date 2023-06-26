b101e5f8734387da421e4da0fa0db7a4e322b71d
Mayesha Hridula <mayesha.fairuz.work@gmail.com>
2023-06-26 16:24:02 +0600

    added changes for better functionality

--- guessTheNumber.js
+++ guessTheNumber.js
@@ -1,7 +1,9 @@
 let guess = prompt("Guess a number between 1 and 10");
 let randomNum = Math.floor((Math.random() * 10) + 1);
 // guess = parseInt(guess);
-if (guess == randomNum) {
+if (guess < 1 || guess > 10) {
+    alert("You guess is out of range, Please try again.");
+} else if (guess == randomNum) {
     alert("You guessed correctly!");
 } else {
     alert("Sorry, The number was " + randomNum + ".");
